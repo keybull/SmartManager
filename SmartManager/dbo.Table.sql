058a6f63c87c147f51dca34b2623722ba52ed7c3
K1ng9 <vkluchak@gmail.com>
2014-12-04 23:54:41 +0200

    Revert "удалить все"
    This reverts commit ad3e1dfb1ec55fc8300b099a4ce6e80c73b9c433.

--- SmartManager/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Tasks]
-(
-	[Id]    INT         NOT NULL,
-    [Stage] NCHAR (255) NULL,
-    [Task]  NCHAR (255) NULL,
-    [Week]  NCHAR (255) NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-)
